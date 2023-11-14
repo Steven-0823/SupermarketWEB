0decfcd8ae444396c778d452037dcba6ff32a09d
joan.henao <henaobermeosteven@gmail.com>
2023-11-14 11:33:28 -0500

    Create Providers

--- Models/Provider.cs
+++ Models/Provider.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
 
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; } = default!;
     }
 }
